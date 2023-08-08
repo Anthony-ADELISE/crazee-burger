a0765c073bb6effa8f0ed5dd780f49a5176f6328
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-09 00:16:24 +0200

    F03 : UI Login Form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "../reusable-ui/Button";
+
+import { PiUserCircleFill } from "react-icons/pi";
 
 const LoginForm = () => {
   const navigate = useNavigate();
@@ -14,23 +17,35 @@ const LoginForm = () => {
   };
 
   return (
-    <section className="bg-red">
-      {/* div titre + sous titre */}
-      <div>
-        <h1>Bienvenue chez nous</h1>
-        <h2>Connectez vous</h2>
+    <section>
+      {/* div titre */}
+      <div className="text-white text-5xl font-amaticSC border-b-2 text-center w-[400px] pb-8 border-[#F56A2C]">
+        <h1>Bienvenue chez nous !</h1>
       </div>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          id="firstName"
-          placeholder="entrez votre prénom"
-          required
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+        <h2 className="text-white text-4xl font-amaticSC text-center pt-10">
+          Connectez vous
+        </h2>
+        <div className="  relative">
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="Entrez votre prénom"
+            required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="inputForm"
+          />
+          <div className="absolute text-greyBlue top-11 left-0 pl-6 flex items-center pointer-events-none">
+            <PiUserCircleFill />
+          </div>
+        </div>
+        <Button
+          className="bg-primary_burger cursor-pointer text-center text-white py-5 mt-5 rounded-md font-openSans"
+          title="Accéder à mon espace"
+          type="submit"
         />
-        <button type="submit">Accédez à votre espace</button>
       </form>
     </section>
   );
--- src/reusable-ui/Logo.jsx
+++ src/reusable-ui/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = ({ className = "" }) => {
     <div
       className={`${className} flex items-center text-primary_burger text-[110px] space-x-5 font-amaticSC`}
     >
-      <h1>Crazy </h1>
+      <h1>Crazee </h1>
       <img className={`w-[200px] ${className}`} src={LogoImg} alt="img logo" />
       <h1>Burger</h1>
     </div>
