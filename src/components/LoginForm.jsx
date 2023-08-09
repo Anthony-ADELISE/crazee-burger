3991282304c56cff1b22d96b1191eb3aa752f3e7
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-09 13:21:03 +0200

    F03: add styling button and remove Button component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -43,7 +43,7 @@ const LoginForm = () => {
             <PiUserCircleFill />
           </div>
         </div>
-        <button className="bg-primary_burger w-full font-bold border border-white font-openSans justify-center space-x-3  text-white py-5 mt-5 rounded-md flex items-center hover:bg-white hover:text-primary_burger hover:border hover:border-primary_burger hover:cursor-pointer transition duration-300">
+        <button className="bg-primary_burger w-full font-bold  font-openSans justify-center space-x-3  text-white py-5 mt-5 rounded-md flex items-center hover:bg-white hover:text-primary_burger hover:border hover:border-primary_burger hover:cursor-pointer transition duration-300">
           <span className="cursor-pointer">Accéder à mon espace</span>
           <BsChevronRight />
         </button>
