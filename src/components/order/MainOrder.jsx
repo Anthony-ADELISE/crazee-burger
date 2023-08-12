489dca003f411e95d25072f891b6b0fab6bad2d4
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-12 12:23:51 +0200

    F05: Import Menu in MainOrder

--- src/components/order/MainOrder.jsx
+++ src/components/order/MainOrder.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Menu from "./menu/Menu";
 
 const MainOrder = () => {
   return (
     <main className="w-full bg-background_white flex-1 rounded-b-lg flex">
       <div className="bg-red w-1/4">Basket</div>
-      <div className="bg-blue w-full">Menu</div>
+      <div className="bg-blue w-full">
+        <Menu />
+      </div>
     </main>
   );
 };
