d37cbaf0570b5eedf9f91804c4207d4ee3be5ce8
Anthony-ADELISE <anthony.adelise@gmail.com>
2023-08-12 12:08:21 +0200

    F05: Add layout MainOrder

--- src/components/order/MainOrder.jsx
+++ src/components/order/MainOrder.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const MainOrder = () => {
   return (
-    <main className="w-full bg-background_white flex-1 rounded-b-lg">ok</main>
+    <main className="w-full bg-background_white flex-1 rounded-b-lg flex">
+      <div className="bg-red w-1/4">Basket</div>
+      <div className="bg-blue w-full">Menu</div>
+    </main>
   );
 };
 
